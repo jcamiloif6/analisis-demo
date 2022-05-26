1349430ff6f0488ae1cd8bf1236134b85727b673
jcamiloif6 <46933022+jcamiloif6@users.noreply.github.com>
2022-05-26 16:06:35 -0500

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <HelloWorld msg="Bienvenido a la página de Métodos Numéricos"/>
   </div>
 </template>
 
