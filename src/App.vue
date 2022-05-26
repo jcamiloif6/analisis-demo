71645d950e405b0bac74fad140f3074fb314389d
jcamiloif6 <46933022+jcamiloif6@users.noreply.github.com>
2022-05-26 16:07:07 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
   <div id="app">
     <nav>
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
       <router-link to="/metodos">Métodos Numéricos</router-link>
     </nav>
     <router-view/>
