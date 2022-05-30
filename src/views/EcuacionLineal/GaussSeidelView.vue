6334623ee297c2dabb64369a911560799ee10363
jcamiloif6 <46933022+jcamiloif6@users.noreply.github.com>
2022-05-30 15:12:47 -0500

    Labels Ecuaciones

--- src/views/EcuacionLineal/GaussSeidelView.vue
+++ src/views/EcuacionLineal/GaussSeidelView.vue
@@ -5,5 +5,15 @@
     <router-link to="/EcuacionLineal/homeEcuacionLineal" tag="button"
       >Otros métodos de Ecuaciones Lineales</router-link
     >
+    <p>Matríz: {{ message }}</p>
+    <input v-model="message" placeholder="" />
+    <p>Vector de Términos Independientes: {{ message }}</p>
+    <input v-model="message" placeholder="" />
+    <p>X inicial: {{ message }}</p>
+    <input v-model="message" placeholder="" />
+    <p>Tolerancia: {{ message }}</p>
+    <input v-model="message" placeholder="" />
+    <p>Número máximo de iterciones: {{ message }}</p>
+    <input v-model="message" placeholder="" />
   </div>
 </template>
